59e67b9ef90fe31129e3913526ac93e39201b569
maqsood <maqsoodhamd37@gmail.com>
2020-10-16 16:37:23 +0530

    [Maqsood].update: added the feature to find the cheapest Hotel for a given date raange based on weekDay and weekend, solved uc4

--- src/main/java/com/bridgeLabs/hotelReservation/HotelReservation.java
+++ src/main/java/com/bridgeLabs/hotelReservation/HotelReservation.java
@@ -4,6 +4,7 @@ import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
@@ -15,99 +16,125 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class HotelReservation {
-	private static final Logger logger=LogManager.getLogger(HotelReservation.class);
+	private static final Logger logger = LogManager.getLogger(HotelReservation.class);
 	private static final Pattern DAY_PATTERN = Pattern.compile("[0-9]{2}[A-Z][a-z]{2}[0-9]{4}");
-	static Scanner sc=new Scanner(System.in);
+	private static final List<DayOfWeek> WEEKENDS = Arrays
+			.asList(new DayOfWeek[] { DayOfWeek.SUNDAY, DayOfWeek.SATURDAY });
+
+	static Scanner sc = new Scanner(System.in);
 	public List<Hotel> hotels;
-	
+
 	public HotelReservation() {
-		this.hotels=new ArrayList<Hotel>();
+		this.hotels = new ArrayList<Hotel>();
 	}
-	
+
 	/**
 	 * uc1
 	 */
 	public void addHotel(String name, int weekdayRate, int weekendRate) {
-		Hotel hotel=new Hotel(name, weekdayRate, weekendRate);
+		Hotel hotel = new Hotel(name, weekdayRate, weekendRate);
 		this.hotels.add(hotel);
 	}
-	
+
 	public Customer getCustomerInput() {
 		logger.info("Enter the date range in format <date1>, <date2>, <date3>\nEg.: 09Mar2020, 10Mar2020, 11Mar2020");
-		String customerInput = sc.nextLine();		
+		String customerInput = sc.nextLine();
 		Matcher dateMatcher = DAY_PATTERN.matcher(customerInput);
-		DateTimeFormatter formatter=DateTimeFormatter.ofPattern("ddMMMuuuu");
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("ddMMMuuuu");
 		List<DayOfWeek> daysList = new ArrayList<DayOfWeek>();
 		while (dateMatcher.find()) {
-			daysList.add(LocalDate.parse(dateMatcher.group(),formatter).getDayOfWeek());
+			daysList.add(LocalDate.parse(dateMatcher.group(), formatter).getDayOfWeek());
 		}
-		int numDays = daysList.size(); 
-		return new Customer(numDays);
+		int numWeekends = (int) daysList.stream().filter(day -> WEEKENDS.contains(day)).count();
+		int numWeekdays = daysList.size() - numWeekends;
+		return new Customer(numWeekdays, numWeekends);
 	}
-	
+
 	public Hotel getCheapestHotel(Customer customer) {
-		int numDays=customer.getNumDays();
-		Map<Hotel, Integer> hotelToTotalRateMap=hotels.stream().collect(Collectors.toMap(hotel->hotel, hotel->hotel.getWeekdayRate()*numDays));
-		Hotel cheapestHotel=hotelToTotalRateMap.keySet().stream().min((n1,n2)->hotelToTotalRateMap.get(n1)-hotelToTotalRateMap.get(n2)).orElse(null);
-		logger.info(cheapestHotel.getName()+", Total Rates: $"+hotelToTotalRateMap.get(cheapestHotel));
+		int numWeekends = customer.getNumWeekends();
+		int numWeekdays = customer.getNumWeekdays();
+		Map<Hotel, Integer> hotelToTotalRateMap = hotels.stream().collect(Collectors.toMap(hotel -> hotel,
+				hotel -> hotel.getWeekendRate() * numWeekends + hotel.getWeekdayRate() * numWeekdays));
+		Hotel cheapestHotel = hotelToTotalRateMap.keySet().stream()
+				.min((n1, n2) -> hotelToTotalRateMap.get(n1) - hotelToTotalRateMap.get(n2)).orElse(null);
+		logger.debug(cheapestHotel.getName() + ", Total Rates: $" + hotelToTotalRateMap.get(cheapestHotel));
 		return cheapestHotel;
 	}
-	
+
 	public static void main(String[] args) {
-		HotelReservation hotelReservation=new HotelReservation();
+		HotelReservation hotelReservation = new HotelReservation();
 		hotelReservation.addHotel("Lakewood", 110, 90);
 		hotelReservation.addHotel("Bridgewood", 150, 50);
 		hotelReservation.addHotel("Ridgewood", 220, 150);
-		hotelReservation.hotels.forEach(hotel->System.out.println(hotel));
+		Customer customer = hotelReservation.getCustomerInput();
+		hotelReservation.getCheapestHotel(customer);
 	}
 }
 
-class Customer{
-	private int numDays;
+class Customer {
+	private int numWeekdays;
+	private int numWeekends;
 
-	public Customer(int numDays) {
+	public Customer(int numWeekdays, int numWeekends) {
 		super();
-		this.numDays = numDays;
+		this.numWeekdays = numWeekdays;
+		this.numWeekends = numWeekends;
 	}
 
-	public int getNumDays() {
-		return numDays;
+	public int getNumWeekends() {
+		return numWeekends;
 	}
 
-	public void setNumDays(int numDays) {
-		this.numDays = numDays;
+	public void setNumWeekends(int numWeekends) {
+		this.numWeekends = numWeekends;
 	}
-	
+
+	public int getNumWeekdays() {
+		return numWeekdays;
+	}
+
+	public void setNumWeekdays(int numWeekdays) {
+		this.numWeekdays = numWeekdays;
+	}
+
 }
 
-class Hotel{
+class Hotel {
 	private String name;
 	private int weekdayRate;
 	private int weekendRate;
+
 	public Hotel(String name, int weekdayRate, int weekendRate) {
 		super();
 		this.name = name;
 		this.weekdayRate = weekdayRate;
 		this.weekendRate = weekendRate;
 	}
+
 	public int getWeekendRate() {
 		return weekendRate;
 	}
+
 	public void setWeekendRate(int weekendRate) {
 		this.weekendRate = weekendRate;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getWeekdayRate() {
 		return weekdayRate;
 	}
+
 	public void setWeekdayRate(int rate) {
 		this.weekdayRate = rate;
 	}
+
 	@Override
 	public String toString() {
 		return "Hotel [\nName=" + name + "\nWeekday Rate=$" + weekdayRate + "\nWeekend Rate=$" + weekendRate + "\n]";
--- src/test/java/com/bridgeLabs/hotelReservation/HotelReservationTest.java
+++ src/test/java/com/bridgeLabs/hotelReservation/HotelReservationTest.java
@@ -6,31 +6,31 @@ import org.junit.Test;
 public class HotelReservationTest {
 	@Test
 	public void givenHotelWhenAddedToHotelReservationSystemShouldHaveTheSame() {
-		HotelReservation hotelReservation=new HotelReservation();
+		HotelReservation hotelReservation = new HotelReservation();
 		hotelReservation.addHotel("Lakewood", 110, 90);
-		String hotelName=hotelReservation.hotels.get(0).getName();
-		int hotelRate=hotelReservation.hotels.get(0).getWeekdayRate();
+		String hotelName = hotelReservation.hotels.get(0).getName();
+		int hotelRate = hotelReservation.hotels.get(0).getWeekdayRate();
 		Assert.assertEquals("Lakewood", hotelName);
 		Assert.assertEquals(110, hotelRate);
 	}
-	
+
 	@Test
 	public void givenHotelsWhenAddedHotelsAndCheapestHotelFoundShouldReturnCheapestHotel() {
-		HotelReservation hotelReservation=new HotelReservation();
+		HotelReservation hotelReservation = new HotelReservation();
 		hotelReservation.addHotel("Lakewood", 110, 90);
 		hotelReservation.addHotel("Bridgewood", 150, 50);
 		hotelReservation.addHotel("Ridgewood", 220, 150);
-		Customer customer=new Customer(2);
-		Hotel cheapestHotel=hotelReservation.getCheapestHotel(customer);
-		Assert.assertEquals("Lakewood",cheapestHotel.getName());
+		Customer customer = new Customer(2, 0);
+		Hotel cheapestHotel = hotelReservation.getCheapestHotel(customer);
+		Assert.assertEquals("Lakewood", cheapestHotel.getName());
 	}
-	
+
 	@Test
 	public void givenHotelWhenAddedToHotelReservationSystemShouldWeekdayAndWeekendRates() {
-		HotelReservation hotelReservation=new HotelReservation();
+		HotelReservation hotelReservation = new HotelReservation();
 		hotelReservation.addHotel("Lakewood", 110, 90);
-		int hotelWeekdayRate=hotelReservation.hotels.get(0).getWeekdayRate();
-		int hotelWeekendRate=hotelReservation.hotels.get(0).getWeekendRate();
+		int hotelWeekdayRate = hotelReservation.hotels.get(0).getWeekdayRate();
+		int hotelWeekendRate = hotelReservation.hotels.get(0).getWeekendRate();
 		Assert.assertEquals(90, hotelWeekendRate);
 		Assert.assertEquals(110, hotelWeekdayRate);
 	}
