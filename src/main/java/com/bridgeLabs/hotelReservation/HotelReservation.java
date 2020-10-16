02101242467aacef42a104e7c2b3bbad25571cfc
maqsood <maqsoodhamd37@gmail.com>
2020-10-16 14:25:49 +0530

    [Maqsood].add: initial commit

---
+++ src/main/java/com/bridgeLabs/hotelReservation/HotelReservation.java
@@ -0,0 +1,12 @@
+package com.bridgeLabs.hotelReservation;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class HotelReservation {
+	private static final Logger logger = LogManager.getLogger(HotelReservation.class);
+
+	public static void main(String[] args) {
+		logger.info("Welcome to Hotel Reservation Program");
+	}
+}
